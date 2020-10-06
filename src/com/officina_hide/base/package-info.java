744ca469f94c3e1960afa3d4c37072538b64aedc
KACOMSAD\ueno <hideo.ueno@kacoms.co.jp>
2020-10-06 21:43:20 +0900

    20/10/06 Updating

---
+++ src/com/officina_hide/base/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * パッケージを運用していく上で共通に使われる機能クラスを管理する。<br>
+ * @author officine-hide.com
+ * @version 1.00
+ * @since 20/10/06
+ */
+package com.officina_hide.base;
---
+++ src/com/officina_hide/base/tools/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * パッケージの構築、基本的なテーブル操作等のクラスを管理する。<br>
+ * @author officine-hide.com
+ * @version 1.00
+ * @since 20/10/06
+ */
+package com.officina_hide.base.tools;
