ad78b57146d44b7520aacf14e575c8f443ebd8c3
KACOMSAD\ueno <hideo.ueno@kacoms.co.jp>
2020-10-15 12:53:17 +0900

    2020/10/15 12:52 リファレンス情報テーブル構築コード完了

--- src/com/officina_hide/base/model/I_FD_Referecne.java
+++ src/com/officina_hide/base/model/I_FD_Referecne.java
@@ -16,4 +16,18 @@ public interface I_FD_Referecne {
 	public final String COMMENT = "リファレンスを管理する為の情報";
 	/**リファレンス情報 : テーブル情報ID */
 	public final int TABLE_ID = 104;
+	
+	//テーブル項目情報
+	/** リファレンス情報ID */
+	public final String COLUMNNAME_FD_Reference_ID = Table_Name + "_ID";
+	public final String NAME_FD_Reference_ID = "リファレンス情報ID";
+	public final String COMMENT_FD_Reference_ID = "リファレンス情報を識別する為の情報ID";
+	/** リファレンス名 */
+	public final String COLUMNNAME_Reference_Name = "Reference_Name";
+	public final String NAME_Reference_Name = "リファレンス名";
+	public final String COMMENT_Reference_Name = "リファレンスを使用する時の名称";
+	/** リファレンス表示名 */
+	public final String COLUMNNAME_FD_Name = "FD_Name";
+	public final String NAME_FD_Name = "リファレンス表示名";
+	public final String COMMENT_FD_Name = "リファレンスを表す名称";
 }
--- src/com/officina_hide/base/tools/FDReference.java
+++ src/com/officina_hide/base/tools/FDReference.java
@@ -1,5 +1,7 @@
 package com.officina_hide.base.tools;
 
+import java.util.Date;
+
 import com.officina_hide.base.common.FD_EnvData;
 import com.officina_hide.base.model.FD_DB;
 import com.officina_hide.base.model.I_FD_Referecne;
@@ -26,6 +28,12 @@ public class FDReference extends FD_DB implements I_FD_Referecne {
 		//テーブル情報の再構築
 		sql = new StringBuffer();
 		sql.append("CREATE TABLE IF NOT EXISTS ").append(Table_Name).append(" (");
+		sql.append(COLUMNNAME_FD_Reference_ID).append(" INT UNSIGNED NOT NULL PRIMARY KEY COMMENT ")
+			.append(FD_SQ).append(NAME_FD_Reference_ID).append(FD_SQ).append(",");
+		sql.append(COLUMNNAME_Reference_Name).append(" Varchar(100) COMMENT ")
+			.append(FD_SQ).append(NAME_Reference_Name).append(FD_SQ).append(",");
+		sql.append(COLUMNNAME_FD_Name).append(" Varchar(100) COMMENT ")
+			.append(FD_SQ).append(NAME_FD_Name).append(FD_SQ).append(",");
 		
 		sql.append(COLUMNNAME_FD_CREATE).append(" DATETIME COMMENT ")
 			.append(FD_SQ).append(NAME_FD_CREATE).append(FD_SQ).append(",");
@@ -37,6 +45,9 @@ public class FDReference extends FD_DB implements I_FD_Referecne {
 			.append(FD_SQ).append(NAME_FD_UPDATED).append(FD_SQ).append(" ");
 		sql.append(") ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT=").append(FD_SQ).append(NAME).append(FD_SQ);
 		DBexecute(env, sql.toString());
+		
+		System.out.println(new Date() + " : " + NAME + "テーブル生成完了");
+
 	}
 
 }
