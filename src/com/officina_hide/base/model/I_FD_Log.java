9b25284821482892f5f9d27c0d724830f01c5ab8
KACOMSAD\ueno <hideo.ueno@kacoms.co.jp>
2020-10-20 12:46:27 +0900

    2020/10/20 ログ情報テーブル構築

---
+++ src/com/officina_hide/base/model/I_FD_Log.java
@@ -0,0 +1,38 @@
+package com.officina_hide.base.model;
+
+/**
+ * ログ情報インターフェース<br>
+ * @author officine-hide.com
+ * @version 1.00
+ * @since 2020/10/20
+ */
+public interface I_FD_Log {
+	
+	/** ログ情報 : テーブル名 */
+	public final String Table_Name = "FD_Log";
+	/** ログ情報 : テーブル表示名 */
+	public final String NAME = "ログ情報";
+	/** ログ情報 : テーブル説明 */
+	public final String COMMENT = "ログを管理する為の情報";
+	/** ログ情報 : テーブル情報ID */
+	public final int TABLE_ID = 105;
+
+	//テーブル項目情報
+	/** ログ情報ID */
+	public final String COLUMNNAME_FD_Log_ID = Table_Name + "_ID";
+	public final String NAME_FD_Log_ID = "ログ情報ID";
+	public final String COMMENT_FD_Log_ID = "ログ情報を識別する為の情報ID";
+	/** ログ種別ID（リファレンス情報ID） */
+	public final String COLUMNNAME_Log_Type_ID = "Log_Type_ID";
+	public final String NAME_Log_Type_ID = "ログ情報種別ID";
+	public final String COMMENT_Log_Type_ID = "ログの種別を識別する為の情報ID（リファレンス情報ID）";
+	/** ログ内容 */
+	/*
+	 * ログの内容には以下のものがある。<br>
+	 * ・(Type = 'Info'(201)) メッセージ...メッセージ情報のIDとパラメータ情報（一部階層化を検討する）
+	 * ・(Type = 'DB'(202)) DB更新内容...ダンプを作る際のベース情報となる。
+	 */
+	public final String COLUMNNAME_Log_Data = "Log_Data";
+	public final String NAME_Log_Data = "ログ内容";
+	public final String COMMENT_Log_Data = "ログの内容を格納する。";
+}
---
+++ src/com/officina_hide/base/tools/FDLog.java
@@ -0,0 +1,63 @@
+package com.officina_hide.base.tools;
+
+import java.util.Date;
+
+import com.officina_hide.base.common.FD_EnvData;
+import com.officina_hide.base.model.FD_DB;
+import com.officina_hide.base.model.I_FD_Log;
+
+/**
+ * ログ情報クラス<br>
+ * @author officine-hide.com
+ * @version 1.00
+ * @since 2020/10/20
+ */
+public class FDLog extends FD_DB implements I_FD_Log {
+
+	/**
+	 * ログ情報構築
+	 * @author officine-hide.com
+	 * @since 1.00 2020/10/20
+	 * @param env 環境情報
+	 */
+	public void createTable(FD_EnvData env) {
+		//テーブル構築
+		createDBTable(env);
+	}
+
+	/**
+	 * ログ情報テーブル構築<br>
+	 * @author officine-hide.com
+	 * @since 1.00 2020/10/20
+	 * @param env 環境情報
+	 */
+	private void createDBTable(FD_EnvData env) {
+		StringBuffer sql = new StringBuffer();
+		//既に登録されているログ情報を削除する。
+		sql.append("DROP TABLE IF EXISTS ").append(Table_Name);
+		DBexecute(env, sql.toString());
+		//ログ情報テーブル構築
+		sql = new StringBuffer();
+		sql.append("CREATE TABLE IF NOT EXISTS ").append(Table_Name).append(" (");
+		sql.append(COLUMNNAME_FD_Log_ID).append(" INT UNSIGNED NOT NULL PRIMARY KEY COMMENT ")
+			.append(FD_SQ).append(NAME_FD_Log_ID).append(FD_SQ).append(",");
+		sql.append(COLUMNNAME_Log_Type_ID).append(" INT UNSIGNED COMMENT ")
+			.append(FD_SQ).append(NAME_Log_Type_ID).append(FD_SQ).append(",");
+		sql.append(COLUMNNAME_Log_Data).append(" TEXT COMMENT ")
+			.append(FD_SQ).append(NAME_Log_Data).append(FD_SQ).append(",");
+		
+		sql.append(COLUMNNAME_FD_CREATE).append(" DATETIME COMMENT ")
+			.append(FD_SQ).append(NAME_FD_CREATE).append(FD_SQ).append(",");
+		sql.append(COLUMNNAME_FD_CREATED).append(" INT UNSIGNED COMMENT ")
+			.append(FD_SQ).append(NAME_FD_CREATED).append(FD_SQ).append(",");
+		sql.append(COLUMNNAME_FD_UPDATE).append(" DATETIME COMMENT ")
+			.append(FD_SQ).append(NAME_FD_UPDATE).append(FD_SQ).append(",");
+		sql.append(COLUMNNAME_FD_UPDATED).append(" INT UNSIGNED COMMENT ")
+			.append(FD_SQ).append(NAME_FD_UPDATED).append(FD_SQ).append(" ");
+		sql.append(") ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT=").append(FD_SQ).append(NAME).append(FD_SQ);
+		DBexecute(env, sql.toString());
+		
+		System.out.println(new Date() + " : " + NAME + "テーブル生成完了");
+	}
+
+}
