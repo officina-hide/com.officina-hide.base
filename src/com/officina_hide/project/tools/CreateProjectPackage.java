9a9d05fed764fc6ee7a5960aaf26e79e842c5e58
KACOMSAD\ueno <hideo.ueno@kacoms.co.jp>
2020-11-05 18:24:54 +0900

    2020/11/05 18:24 専用プロセスID付与、プロセス生成メソッド開発待ち

--- src/com/officina_hide/project/tools/CreateProjectPackage.java
+++ src/com/officina_hide/project/tools/CreateProjectPackage.java
@@ -1,6 +1,9 @@
 package com.officina_hide.project.tools;
 
-import com.officina_hide.base.tools.FDProcess;
+import java.io.File;
+import java.io.IOException;
+
+import com.officina_hide.base.common.FD_EnvData;
 
 /**
  * プロジェクトパッケージのベースを構築する。<br>
@@ -10,10 +13,26 @@ import com.officina_hide.base.tools.FDProcess;
  */
 public class CreateProjectPackage {
 
+	private static int thisPorcessId = 201;
+	
 	public static void main(String[] args) {
+		
+		//環境情報のPathを設定する。
+		FD_EnvData env = null;
+		try {
+			String propPath = new File(".").getCanonicalPath()+"\\data\\base.properties";
+			//環境情報の取込
+			env = new FD_EnvData(propPath);
+			env.setActiveProcessID(thisPorcessId);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-		//プロセス情報登録
-		FDProcess process = new FDProcess();
+		//プロセス情報ID付きのプロセス生成メソッド作成
+//		//プロセス情報登録
+//		FDProcess process = new FDProcess();
+//		int processId = process.createProcess(env, CreateProjectPackage.class.getSimpleName());
+//		env.setActiveProcessID(processId);
 	}
 
 }
