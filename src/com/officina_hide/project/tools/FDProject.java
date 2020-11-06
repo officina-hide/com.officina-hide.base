84cceae04d11d40cc37aad7489f78cdc32a0c19c
KACOMSAD\ueno <hideo.ueno@kacoms.co.jp>
2020-11-06 17:24:46 +0900

    2020/11/06 Updated

--- src/com/officina_hide/project/tools/FDProject.java
+++ src/com/officina_hide/project/tools/FDProject.java
@@ -2,7 +2,9 @@ package com.officina_hide.project.tools;
 
 import com.officina_hide.base.common.FD_EnvData;
 import com.officina_hide.base.model.FD_DB;
+import com.officina_hide.base.model.I_FD_Log;
 import com.officina_hide.base.tools.FDTable;
+import com.officina_hide.base.tools.FDTableColumn;
 import com.officina_hide.project.model.I_FD_Project;
 
 /**
@@ -20,9 +22,24 @@ public class FDProject extends FD_DB implements I_FD_Project {
 	 * @param env 環境情報
 	 */
 	public void createTable(FD_EnvData env) {
+		addLog(env, I_FD_Log.LOGTYPE_Info_ID, NAME+"テーブル構築開始");
+		
 		//テーブル情報登録
 		FDTable table = new FDTable();
 		table.addData(env, TABLE_ID, Table_Name, NAME, COMMENT);
+		//テーブル項目情報登録
+		FDTableColumn column = new FDTableColumn();
+		
+		column.addData(env, 0, TABLE_ID, COLUMNNAME_FD_Process_ID, NAME_FD_Process_ID, COMMENT_FD_Process_ID
+				, COLUMNTYPE_ID_FD_Information_ID, null, 0, 900, "N", "N");
+		column.addData(env, 0, TABLE_ID, COLUMNNAME_FD_CREATE, NAME_FD_CREATE, COMMENT_FD_CREATE
+				, COLUMNTYPE_ID_FD_Date, null, 0, 910, "N", "N");
+		column.addData(env, 0, TABLE_ID, COLUMNNAME_FD_CREATED, NAME_FD_CREATED, COMMENT_FD_CREATED
+				, COLUMNTYPE_ID_FD_Information_ID, null, 0, 920, "N", "N");
+		column.addData(env, 0, TABLE_ID, COLUMNNAME_FD_UPDATE, NAME_FD_UPDATE, COMMENT_FD_UPDATE
+				, COLUMNTYPE_ID_FD_Date, null, 0, 930, "N", "N");
+		column.addData(env, 0, TABLE_ID, COLUMNNAME_FD_UPDATED, NAME_FD_UPDATED, COMMENT_FD_UPDATED
+				, COLUMNTYPE_ID_FD_Information_ID, null, 0, 940, "N", "N");
 	}
 
 }
