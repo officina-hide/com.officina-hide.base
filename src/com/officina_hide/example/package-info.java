0aa0639478b6ee3aea1ba49a50ca83a82f88d13a
KACOMSAD\ueno <hideo.ueno@kacoms.co.jp>
2020-11-16 12:57:01 +0900

    2020/11/16 12:56 データベースのユーザー変更

---
+++ src/com/officina_hide/example/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * 使用例を保管する<br>
+ * CREATE USER 'testuser'@'%' IDENTIFIED BY 'Test*User001';
+ * GRANT ALL PRIVILEGES ON datatest.* to 'testuser'@'%' with Grant option;
+ * <p>2020/11/16[main] データベースのユーザーを変更（rootは使わない）
+ * 環境情報を暗号化する。</p>
+ * @author officine-hide.com
+ * @version 1.20 新規作成
+ * @since 2020/11/16
+ */
+package com.officina_hide.example;
