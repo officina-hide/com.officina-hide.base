524f6ca84f75104912676bab280b27cfb6e60817
KACOMSAD\ueno <hideo.ueno@kacoms.co.jp>
2020-11-16 17:27:29 +0900

    2020/11/16 17:27 Updated

--- src/com/officina_hide/example/package-info.java
+++ src/com/officina_hide/example/package-info.java
@@ -3,7 +3,17 @@
  * CREATE USER 'testuser'@'%' IDENTIFIED BY 'Test*User001';
  * GRANT ALL PRIVILEGES ON datatest.* to 'testuser'@'%' with Grant option;
  * <p>2020/11/16[main] データベースのユーザーを変更（rootは使わない）
- * 環境情報を暗号化する。</p>
+ * 環境情報を暗号化する。<br>
+ * パッケージを展開していく上での課題を出すために、自分用のサンプルをいくつか構築する。<br>
+ * 0. パッケージ構築管理 → (Package Management) PMSS<br>
+ * 　　　ベースとして構築していく。<br>
+ * 1. 会計処理（Version1.0は個人用）<br>
+ * 2. プロジェクト管理（火星探検見積）<br>
+ * 3. 教育用（以前作ったnklickの汎用版）<br>
+ * 4. 生産管理（松下の冷蔵庫工場をイメージした管理）<br>
+ * 5. 設備管理（発電所のPDCAをイメージした管理）<br>
+ * 6. 医療事務<br>
+ * 7. ドキュメント管理（他のプロジェクトで使用できる汎用型を考察）<br></p>
  * @author officine-hide.com
  * @version 1.20 新規作成
  * @since 2020/11/16
