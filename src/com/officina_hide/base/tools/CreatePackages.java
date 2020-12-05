3256080f38fde2f896518c547281d9ba27910b40
KACOMSAD\ueno <hideo.ueno@kacoms.co.jp>
2020-12-05 18:25:12 +0900

    2020/12/05 18:24 パッケージ生成開始

---
+++ src/com/officina_hide/base/tools/CreatePackages.java
@@ -0,0 +1,24 @@
+package com.officina_hide.base.tools;
+
+/**
+ * パッケージ生成[Package generation]<br>
+ * <p>本クラスではパッケージの生成を行う。<br>
+ * [This class creates a package.]</p>
+ * @author officine-hide.com
+ * @version 1.30
+ * @since 2020/12/05
+ */
+public class CreatePackages {
+
+	public static void main(String[] args) {
+		/*
+		 * 環境情報の取得[Acquisition of environmental information]
+		 */
+		
+		/*
+		 * 基盤テーブルの構築[Building a base table]
+		 */
+//		CreateBaseInformation(env);
+	}
+
+}
---
+++ src/com/officina_hide/base/tools/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * プロジェクト構成する基盤情報を取り扱うクラスを管理する。<br>
+ * <p>Manage the classes that handle the basic information that makes up the project.</p>
+ */
+package com.officina_hide.base.tools;
