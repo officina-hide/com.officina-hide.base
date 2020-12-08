1bd16ebdfe228661e04b407f09c7455bfbdd6af8
KACOMSAD\ueno <hideo.ueno@kacoms.co.jp>
2020-12-08 11:57:01 +0900

    2020/12/08 グラフィック研究用のworkshopパッケージ構築

---
+++ src/com/officina_hide/workshop/graphics/basetest/Test01.java
@@ -0,0 +1,17 @@
+package com.officina_hide.workshop.graphics.basetest;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Test01 extends Application {
+
+	@Override
+	public void start(Stage stage) throws Exception {
+
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
---
+++ src/com/officina_hide/workshop/graphics/basetest/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 基本的な動作確認をする為のクラスを管理する。<br>
+ * Manage classes for basic operation check.<br>
+ * @author officine-hide.com
+ * @version 1.00
+ * @since 2020/12/08
+ */
+package com.officina_hide.workshop.graphics.basetest;
