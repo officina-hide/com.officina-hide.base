b9182d9968c0b41c2d1337414e9796970903a0da
KACOMSAD\ueno <hideo.ueno@kacoms.co.jp>
2020-12-09 09:33:49 +0900

    2020/12/09 Graphic Test 開始

--- src/com/officina_hide/workshop/graphics/basetest/Test01.java
+++ src/com/officina_hide/workshop/graphics/basetest/Test01.java
@@ -1,12 +1,34 @@
 package com.officina_hide.workshop.graphics.basetest;
 
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class Test01 extends Application {
 
 	@Override
 	public void start(Stage stage) throws Exception {
+		Group root = new Group();
+		
+		Canvas canvas = new Canvas(100, 100);
+		root.getChildren().add(canvas);
+		
+		GraphicsContext gc = canvas.getGraphicsContext2D();
+		gc.setStroke(Color.GRAY);
+		gc.strokeRect(0, 0, 100, 100);
+		Rectangle rect = new Rectangle(10, 10, 30, 30);
+		root.getChildren().add(rect);
+		rect.setOnMouseClicked(event->{
+			System.out.println("cliscked");
+		});
+		
+		Scene scene = new Scene(root, 400, 200);
+		stage.setScene(scene);
 		stage.show();
 	}
 
