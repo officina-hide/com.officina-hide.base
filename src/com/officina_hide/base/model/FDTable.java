849cef6dd3b4a9dac93cdc390ec91c190055aae3
KACOMSAD\ueno <hideo.ueno@kacoms.co.jp>
2020-12-10 10:04:23 +0900

    2020/12/10 1004 テーブル情報構築完了

--- src/com/officina_hide/base/model/FDTable.java
+++ src/com/officina_hide/base/model/FDTable.java
@@ -1,5 +1,6 @@
 package com.officina_hide.base.model;
 
+import com.officina_hide.base.common.FD_DB_Utility;
 import com.officina_hide.base.common.FD_EnvData;
 
 /**
@@ -23,6 +24,25 @@ public class FDTable extends FD_DB implements I_FD_Table {
 		sqlDrop.append("DROP TABLE IF EXISTS ").append(Table_Name);
 		DBUpdateExecution(env, sqlDrop.toString());
 	
+		//テーブル情報の再構築
+		sql.append("CREATE TABLE IF NOT EXISTS ").append(Table_Name).append(" (");
+		sql.append(COLUMNNAME_FD_Table_ID).append(" INT UNSIGNED NOT NULL PRIMARY KEY COMMENT ")
+			.append(FD_SQ).append(NAME_FD_Table_ID).append(FD_SQ).append(",");
+		sql.append(COLUMNNAME_Table_Name).append(" Varchar(100) COMMENT ")
+			.append(FD_SQ).append(NAME_Table_Name).append(FD_SQ).append(",");
+		sql.append(COLUMNNAME_FD_Name).append(" Varchar(100) COMMENT ")
+			.append(FD_SQ).append(NAME_FD_Name).append(FD_SQ).append(",");
+		sql.append(COLUMNNAME_FD_Comment).append(" Text COMMENT ")
+			.append(FD_SQ).append(NAME_FD_Comment).append(FD_SQ).append(",");
+		
+		FD_DB_Utility dutil = new FD_DB_Utility();
+		sql.append(dutil.getBaseSQLStrings(NAME));		
+		DBUpdateExecution(env, sql.toString());
+		
+		//ログ情報の構築をログ情報に登録する。
+		FDLog log = new FDLog();
+		log.addLog(env, I_FD_Log.LOGTYPE_Table_Drop_ID, changeEscape(sqlDrop.toString()));
+		log.addLog(env, I_FD_Log.LOGTYPE_Table_Create_ID, changeEscape(sql.toString()));
 	}
 
 }
