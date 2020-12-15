aa9caa3cd75bf9708471255c7bb6eba7a1830c23
hideo ueno <hideo.ueno@kacoms.co.jp>
2020-12-15 10:51:45 +0900

    2020/12/15 テーブル項目情報テーブル生成

--- src/com/officina_hide/base/model/FDTableColumn.java
+++ src/com/officina_hide/base/model/FDTableColumn.java
@@ -1,5 +1,6 @@
 package com.officina_hide.base.model;
 
+import com.officina_hide.base.common.FD_DB_Utility;
 import com.officina_hide.base.common.FD_EnvData;
 
 /**
@@ -26,6 +27,43 @@ public class FDTableColumn extends FD_DB implements I_FD_TableColumn {
 		//既に登録されているテーフル情報を削除する。
 		sqlDrop.append("DROP TABLE IF EXISTS ").append(Table_Name);
 		DBUpdateExecution(env, sqlDrop.toString());
+
+		//テーブル情報の再構築
+		sql.append("CREATE TABLE IF NOT EXISTS ").append(Table_Name).append(" (");
+		sql.append(COLUMNNAME_FD_TableColumn_ID).append(" INT UNSIGNED NOT NULL PRIMARY KEY COMMENT ")
+			.append(FD_SQ).append(NAME_FD_TableColumn_ID).append(FD_SQ).append(",");
+		sql.append(COLUMNNAME_FD_Table_ID).append(" INT UNSIGNED COMMENT ")
+			.append(FD_SQ).append(NAME_FD_Table_ID).append(FD_SQ).append(",");
+		sql.append(COLUMNNAME_TableColumn_Name).append(" VARCHAR(100) COMMENT ")
+			.append(FD_SQ).append(NAME_TableColumn_Name).append(FD_SQ).append(",");
+		sql.append(COLUMNNAME_FD_Name).append(" VARCHAR(100) ")
+			.append(" COMMENT ").append(FD_SQ).append(NAME_FD_Name).append(FD_SQ).append(",");
+		sql.append(COLUMNNAME_FD_Comment).append(" TEXT ")
+			.append(" COMMENT ").append(FD_SQ).append(NAME_FD_Comment).append(FD_SQ).append(",");
+		sql.append(COLUMNNAME_TableColumn_Type_ID).append(" INT UNSIGNED COMMENT ")
+			.append(FD_SQ).append(NAME_TableColumn_Type_ID).append(FD_SQ).append(",");
+		sql.append(COLUMNNAME_TableColumn_Size).append(" INT UNSIGNED COMMENT ")
+			.append(FD_SQ).append(NAME_TableColumn_Size).append(FD_SQ).append(",");
+		sql.append(COLUMNNAME_Defualt_Data).append(" VARCHAR(100) COMMENT ")
+			.append(FD_SQ).append(NAME_Defualt_Data).append(FD_SQ).append(",");
+		sql.append(COLUMNNAME_Column_Sort_Order).append(" INT UNSIGNED COMMENT ")
+			.append(FD_SQ).append(NAME_Column_Sort_Order).append(FD_SQ).append(",");
+		sql.append(COLUMNNAME_Is_Null).append(" ENUM (")
+			.append(FD_SQ).append("Y").append(FD_SQ).append(",")
+			.append(FD_SQ).append("N").append(FD_SQ).append(")  ")
+			.append("COMMENT ").append(FD_SQ).append(NAME_Is_Null).append(FD_SQ).append(",");
+		sql.append(COLUMNNAME_Is_Primary).append(" ENUM(")
+			.append(FD_SQ).append("Y").append(FD_SQ).append(",")
+			.append(FD_SQ).append("N").append(FD_SQ).append(")  ")
+			.append("COMMENT ").append(FD_SQ).append(NAME_Is_Primary).append(FD_SQ).append(",");
+		
+		FD_DB_Utility dutil = new FD_DB_Utility();
+		sql.append(dutil.getBaseSQLStrings(NAME));		
+		DBUpdateExecution(env, sql.toString());
+		
+		//ログ情報の構築をログ情報に登録する。
+		log.addLog(env, I_FD_Log.LOGTYPE_Table_Drop_ID, changeEscape(sqlDrop.toString()));
+		log.addLog(env, I_FD_Log.LOGTYPE_Table_Create_ID, changeEscape(sql.toString()));
 
 		log.addLog(env, I_FD_Log.LOGTYPE_Info_ID, NAME+"テーブル構築完了");
 	}
