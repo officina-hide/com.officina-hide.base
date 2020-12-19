71693595361b4dc497ee6c1a9829d4e795478d3a
hideo ueno <hideo.ueno@kacoms.co.jp>
2020-12-19 18:01:30 +0900

    2020/12/19 一覧テーブル上にCheckBoxとComboBoxを表示できるようにする、データの取得が課題

--- src/com/officina_hide/workshop/task/tasklist/FX_TaskList.java
+++ src/com/officina_hide/workshop/task/tasklist/FX_TaskList.java
@@ -8,11 +8,17 @@ import java.util.Locale;
 import com.officina_hide.base.common.FD_Date;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.CheckBoxTableCell;
+import javafx.scene.control.cell.ComboBoxTableCell;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -20,26 +26,54 @@ public class FX_TaskList extends Application {
 
 	@Override
 	public void start(Stage stage) throws Exception {
-		VBox root = new VBox();
+		VBox root = new VBox(5);
 		root.setPadding(new Insets(5, 5, 5, 5));
+		root.setStyle("-fx-font-size:12.0;-fx-font-family:MeiryoUI");
 		
 		/*
-		 * 日付項目については、表示の書式を設定する関係上、テーブル項目用のクラスが必要
+		 * 日付項目については、表示の書式を設定する関係上、テーブル項目用のクラス(FD_Date)を作成。<br>
+		 * 
 		 */
 		
 		TableView<TaskTableData> table = new TableView<TaskTableData>();
+		TableColumn<TaskTableData, Boolean> checkCol = new TableColumn<TaskTableData, Boolean>("check");
+		TableColumn<TaskTableData, String> comboCol = new TableColumn<>("状況");
 		TableColumn<TaskTableData, String> titleCol = new TableColumn<TaskTableData, String>("タイトル");
 		TableColumn<TaskTableData, FD_Date> sdateCol = new TableColumn<TaskTableData, FD_Date>("開始日");
+		
+		ObservableList<String> list = FXCollections.observableArrayList("OK","ERROR");
+		
+		comboCol.setCellValueFactory(new PropertyValueFactory<TaskTableData, String>("status"));
+		comboCol.setCellFactory(ComboBoxTableCell.forTableColumn(list));
+		comboCol.setPrefWidth(100);
+
+		
+		
+		checkCol.setCellValueFactory(new PropertyValueFactory<TaskTableData, Boolean>("check"));
+		checkCol.setCellFactory(CheckBoxTableCell.forTableColumn(checkCol));	
 		titleCol.setCellValueFactory(new PropertyValueFactory<TaskTableData, String>("title"));
+		titleCol.setCellFactory(TextFieldTableCell.forTableColumn());
+		titleCol.setPrefWidth(100);
 		sdateCol.setCellValueFactory(new PropertyValueFactory<TaskTableData, FD_Date>("startDate"));
+		table.getColumns().add(checkCol);
+		table.getColumns().add(comboCol);
 		table.getColumns().add(titleCol);
 		table.getColumns().add(sdateCol);
 		root.getChildren().add(table);
+		table.setEditable(true);
 		
+		//ボタン追加
+		Button button = new Button("登録");
+		root.getChildren().add(button);
+		button.setOnMouseClicked(event->{
+		});
+		
+		//データ追加
 		Calendar cal = new GregorianCalendar(new Locale("ja", "JP"));
 		cal.setTime(new Date());
 		TaskTableData data01 = new TaskTableData("AAAAA", cal);
 		table.getItems().add(data01);	
+
 		Scene scene = new Scene(root, 400, 300);
 		stage.setScene(scene);
 		stage.show();
--- src/com/officina_hide/workshop/task/tasklist/TaskTableData.java
+++ src/com/officina_hide/workshop/task/tasklist/TaskTableData.java
@@ -17,6 +17,8 @@ public class TaskTableData {
 		startDate.setDate(start_Date);
 	}
 
+	private boolean check;
+	private String status;
 	private String title;
 	private FD_Date startDate = new FD_Date();
 	
@@ -34,5 +36,29 @@ public class TaskTableData {
 	}
 	public FD_Date getStartDate() {
 		return startDate;
+	}
+	/**
+	 * @return check
+	 */
+	public boolean isCheck() {
+		return check;
+	}
+	/**
+	 * @param check セットする check
+	 */
+	public void setCheck(boolean check) {
+		this.check = check;
+	}
+	/**
+	 * @return status
+	 */
+	public String getStatus() {
+		return status;
+	}
+	/**
+	 * @param status セットする status
+	 */
+	public void setStatus(String status) {
+		this.status = status;
 	}
 }
