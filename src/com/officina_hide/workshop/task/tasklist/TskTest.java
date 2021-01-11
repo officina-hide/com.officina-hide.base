2a564b71b6a59963a48b056b4245b193f33958ea
ueno-XPS\hideo <hideo@uenotks.com>
2021-01-11 18:54:50 +0900

    タスクリストテストクラス構築開始

---
+++ src/com/officina_hide/workshop/task/tasklist/TskTest.java
@@ -0,0 +1,23 @@
+package com.officina_hide.workshop.task.tasklist;
+
+/**
+ * タスク情報のテストを行う。<br>
+ * Test task information.
+ * @author officina-hide.com
+ * @version 1.30
+ * @since 2021/01/11
+ */
+public class TskTest {
+
+	public static void main(String[] args) {
+		/*
+		 * タスク情報を登録する。
+		 */
+		
+		//タスク情報一覧画面表示
+//		FX_TaskList taskList = new FX_TaskList();
+		FX_TaskList.main(null);
+		System.out.println("test");
+	}
+
+}
