208b9d3b6e6c74c84e35d1a46c217cdb92c1298c
hideo ueno <hideo.ueno@kacoms.co.jp>
2021-02-02 10:37:44 +0900

    Task照会画面更新中

--- src/com/officina_hide/workshop/task/tasklist/FX_TaskView.java
+++ src/com/officina_hide/workshop/task/tasklist/FX_TaskView.java
@@ -7,10 +7,15 @@ import javafx.stage.Stage;
 
 public class FX_TaskView extends Application {
 
+	/** 環境情報 */
 	private FD_EnvData env;
 	
 	@Override
 	public void start(Stage stage) throws Exception {
+		/*
+		 * 本画面が呼び出される際に対象テーブルの情報IDがセットされていた場合は、既登録の情報を抽出し画面にセットする。
+		 * 画面にセットする項目は、画面情報・画面項目情報から取得する。
+		 */
 		stage.showAndWait();
 	}
 
