5b011d2651a67536b604d8a9ebc067ec05665de9
hideo ueno <hideo.ueno@kacoms.co.jp>
2021-02-02 12:57:34 +0900

    Fx用基盤構築クラス生成

---
+++ src/com/officina_hide/base/tools/CreateFxBaseInformation.java
@@ -0,0 +1,12 @@
+package com.officina_hide.base.tools;
+
+/**
+ * Fx画面用基盤構築<br>
+ * Infrastructure construction for Fx screen.
+ * @author officine-hide.com
+ * @version 1.31
+ * @since 2021/02/02
+ */
+public class CreateFxBaseInformation {
+
+}
---
+++ src/com/officina_hide/fx/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Fx画面用クラスを管理する。<br>
+ * Manage Fx screen classes.
+ * @author officine-hide.com
+ * @since 2021/02/02
+ */
+package com.officina_hide.fx;
