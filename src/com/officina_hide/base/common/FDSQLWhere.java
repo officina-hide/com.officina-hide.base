7b1ca3bc324fb0d465e097fa60d68699645c509a
hideo ueno <hideo.ueno@kacoms.co.jp>
2021-02-09 09:36:49 +0900

    Where句用クラス構築(コンストラクター時の機能生成)

--- src/com/officina_hide/base/common/FDSQLWhere.java
+++ src/com/officina_hide/base/common/FDSQLWhere.java
@@ -1,21 +1,26 @@
 package com.officina_hide.base.common;
 
+import com.officina_hide.base.model.I_FD_DB;
+
 /**
  * SQL用whereクラス[Where class for SQL]<br>
  * @author officine-hide.com
  * @version 1.31
  * @since 2021/02/09
  */
-public class FDSQLWhere {
+public class FDSQLWhere implements I_FD_DB {
 
+	private StringBuffer where;
+	
 	/**
 	 * コンストラクター<br>
-	 * 
-	 * @param columnName
-	 * @param value
+	 * 実体化時に、指定された項目に対して文字情報のwhere句を生成する。<br>
+	 * At the time of instance, the where clause of character information is generated for the specified item.
+	 * @param columnName 項目名
+	 * @param value 文字情報
 	 */
 	public FDSQLWhere(String columnName, String value) {
-		
+		where.append(columnName).append(" = ").append(FD_SQ).append(value).append(FD_SQ);
 	}
 	
 }
