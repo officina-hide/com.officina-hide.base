a4bb291aadb9aafd4df7618c531fca80f89dae64
hideo ueno <hideo.ueno@kacoms.co.jp>
2021-02-09 09:11:09 +0900

    Updated

---
+++ src/com/officina_hide/base/common/FDSQLWhere.java
@@ -0,0 +1,21 @@
+package com.officina_hide.base.common;
+
+/**
+ * SQL用whereクラス[Where class for SQL]<br>
+ * @author officine-hide.com
+ * @version 1.31
+ * @since 2021/02/09
+ */
+public class FDSQLWhere {
+
+	/**
+	 * コンストラクター<br>
+	 * 
+	 * @param columnName
+	 * @param value
+	 */
+	public FDSQLWhere(String columnName, String value) {
+		
+	}
+	
+}
