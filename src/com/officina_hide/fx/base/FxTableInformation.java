bffc63d74b8684e269006220f5187e04bbb4afb4
hideo ueno <hideo.ueno@kacoms.co.jp>
2021-07-12 13:03:51 +0900

    Updated

--- src/com/officina_hide/fx/base/FxTableInformation.java
+++ src/com/officina_hide/fx/base/FxTableInformation.java
@@ -1,6 +1,11 @@
 package com.officina_hide.fx.base;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.officina_hide.base.common.FD_EnvData;
+import com.officina_hide.base.model.FD_DB;
+import com.officina_hide.base.model.X_FD_Table;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
@@ -17,6 +22,8 @@ import javafx.stage.Stage;
  * @since 2021/07/06
  */
 public class FxTableInformation extends Application {
+	/** DBクラス */
+	private FD_DB DB = new FD_DB();
 	/** 環境情報 */
 	private FD_EnvData env = new FD_EnvData();
 
@@ -33,6 +40,9 @@ public class FxTableInformation extends Application {
 
 	@Override
 	public void start(Stage stage) throws Exception {
+		//テーブル情報取得
+		List<X_FD_Table> list = getTableList();
+		
 		VBox root = new VBox(5);
 		TableView<String> table = new TableView<>();
 		root.getChildren().add(table);
@@ -46,6 +56,18 @@ public class FxTableInformation extends Application {
 		stage.setScene(scene);
 		//画面をユーザー対応待ちとして表示する。
 		stage.show();
+	}
+
+	/**
+	 * テーブル情報リスト取得[Get table information list]<br>
+	 * @author officine-hide.net
+	 * @since 1.00 2021/07/12
+	 * @return テーブル情報リスト
+	 */
+	private List<X_FD_Table> getTableList() {
+		List<X_FD_Table> list = new ArrayList<>();
+//		List<Integer> ids = DB.getAllId(env);
+		return list;
 	}
 
 }
