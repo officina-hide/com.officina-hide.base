2bdde1617662ea05fe74543ea7313d0ce050598a
hideo ueno <hideo.ueno@kacoms.co.jp>
2021-08-05 13:06:32 +0900

    Added "Display" button to the table list.
    テーブル一覧に「表示」ボタン追加。

--- src/com/officina_hide/fx/base/FxTableInformation.java
+++ src/com/officina_hide/fx/base/FxTableInformation.java
@@ -11,10 +11,16 @@ import com.officina_hide.base.model.I_FD_Table;
 import com.officina_hide.base.model.X_FD_Table;
 
 import javafx.application.Application;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.MapValueFactory;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -29,6 +35,13 @@ public class FxTableInformation extends Application {
 	private FD_DB DB = new FD_DB();
 	/** 環境情報 */
 	private FD_EnvData env = new FD_EnvData();
+	/** 一覧ビュー */
+	@SuppressWarnings("rawtypes")
+	private TableView<Map> table;
+	/** 「表示」ボタン */
+	private Button dispButton;
+	/** テーブル選択位置 */
+	private int viewNo = -1;
 
 	/**
 	 * インスタンス時に、環境情報をセットする。<br>
@@ -47,9 +60,20 @@ public class FxTableInformation extends Application {
 		//テーブル情報取得
 		List<X_FD_Table> list = getTableList();
 		VBox root = new VBox(5);
-		TableView<Map> table = new TableView<>();
-		root.getChildren().add(table);
+		root.setPadding(new Insets(5, 5, 5, 5));
 		
+		//ボタン領域表示
+		HBox buttonArea = new HBox(5);
+		root.getChildren().add(buttonArea);
+		dispButton = new Button("表示");
+		buttonArea.getChildren().add(dispButton);
+		dispButton.setDisable(true);
+		dispButton.setOnAction(event->{
+			dispButtonSelected(event);
+		});
+		
+		table = new TableView<>();
+		root.getChildren().add(table);
 		TableColumn<Map, String> TableName = new TableColumn<>("テーブル物理名");
 		TableName.setCellValueFactory(new MapValueFactory<>(I_FD_Table.COLUMNNAME_FD_Table_Name));
 		TableColumn<Map, String> Name = new TableColumn<>("テーブル表示名");
@@ -69,19 +93,44 @@ public class FxTableInformation extends Application {
 			table.getItems().add(map);
 		}
 		table.setOnMouseClicked(event ->{
-			if(event.getClickCount() == 2) {
-				System.out.println("Double Clicked!!");
-				System.out.println(table.getSelectionModel().getSelectedIndex());
-				System.out.println(table.getItems().get(table.getSelectionModel().getSelectedIndex()));
-				Map map = table.getItems().get(table.getSelectionModel().getSelectedIndex());
-				System.out.println(map.get(I_FD_Table.COLUMNNAME_FD_Table_Name));
-			}
+			tableclicked(event);
 		});
 		
 		Scene scene = new Scene(root, 500, 300);
 		stage.setScene(scene);
 		//画面をユーザー対応待ちとして表示する。
 		stage.show();
+	}
+
+	/**
+	 * 「表示」ボタン選択時処理[Processing when "Display" button is selected]<br>
+	 * @author officine-hide.net
+	 * @since 1.00 2021/08/05
+	 * @param event イベント情報
+	 */
+	@SuppressWarnings("rawtypes")
+	private void dispButtonSelected(ActionEvent event) {
+		if(viewNo == -1) {
+			return;
+		}
+		ObservableList<Map> items = table.getSelectionModel().getSelectedItems();
+		Map map = items.get(0);
+		System.out.println(map.get(I_FD_Table.COLUMNNAME_FD_Table_ID));
+	}
+
+	/**
+	 * テーブルクリック時処理[Processing when clicking a table]<br>
+	 * @author officine-hide.net
+	 * @since 1.00 2021/08/05
+	 * @param event イベント情報
+	 */
+	private void tableclicked(MouseEvent event) {
+		System.out.println("Clicked!!");
+		//「表示」ボタン活性化
+		dispButton.setDisable(false);
+		//選択位置の保管
+		viewNo = table.getSelectionModel().getSelectedIndex();
+		System.out.println(viewNo);
 	}
 
 	/**
