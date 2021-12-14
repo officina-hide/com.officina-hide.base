830b0f7e16c5bf74353ab974be29dc32d5800ef9
hideo ueno <hideo.ueno@kacoms.co.jp>
2021-12-14 12:01:06 +0900

    Reference variable information table construction completed
    参照変数情報テーブル構築完了

--- src/com/officina_hide/base/model/FD_Reference_Param.java
+++ src/com/officina_hide/base/model/FD_Reference_Param.java
@@ -20,6 +20,28 @@ public class FD_Reference_Param extends FD_DB implements I_FD_Reference_Param {
 		//テーブル情報登録
 		FD_Table table = new FD_Table();
 		table.add(env, Table_ID, Table_Name, Table_Disp_Name, Table_Comment);
+		//採番情報登録
+		FD_Numbering num = new FD_Numbering();
+		num.add(env, Table_ID, Table_ID, 100001, 0);
+		//辞書情報登録
+		FD_DataDictionary dd = new FD_DataDictionary();
+		dd.add(env, 0, COLUMNNAME_FD_Reference_Param_ID, NAME_FD_Reference_Param_ID, COMMENT_FD_Reference_Param_ID);
+		dd.add(env, 0, COLUMNNAME_FD_Reference_Param_Name, NAME_FD_Reference_Param_Name, COMMENT_FD_Reference_Param_Name);
+		dd.add(env, 0, COLUMNNAME_FD_Reference_Param_Numeric, NAME_FD_Reference_Param_Numeric, COMMENT_FD_Reference_Param_Numeric);
+		//テーブル項目情報登録
+		FD_Column column = new FD_Column();
+		column.add(env, 0, Table_ID, COLUMNNAME_FD_Reference_Param_ID, FD_ITEM_ID, 0, false, true, "0");
+		column.add(env, 0, Table_ID, COLUMNNAME_FD_Reference_Param_Name, FD_ITEM_String, 100, true, false, null);
+		column.add(env, 0, Table_ID, COLUMNNAME_FD_Reference_ID, FD_ITEM_ID, 0, false, false, "0");
+		column.add(env, 0, Table_ID, COLUMNNAME_FD_TypeItem_ID, FD_ITEM_ID, 0, true, false, "0");
+		column.add(env, 0, Table_ID, COLUMNNAME_FD_Reference_Param_Numeric, FD_ITEM_BigInt, 0, true, false, "0");
+		column.add(env, 0, Table_ID, COLUMNNAME_FD_Name, FD_ITEM_String, 100, true, false, null);
+		column.add(env, 0, Table_ID, COLUMNNAME_FD_Description, FD_ITEM_Text, 0, true, false, null);
+		addCommonColumn(env, Table_ID);
+		//テーブル削除
+		dropTable(env, Table_Name);
+		//テーブル生成
+		createTable(env, Table_Name);
 	}
 
 }
--- src/com/officina_hide/base/model/I_FD_Reference_Param.java
+++ src/com/officina_hide/base/model/I_FD_Reference_Param.java
@@ -19,4 +19,22 @@ public interface I_FD_Reference_Param extends I_FD_DB {
 	/** テーブルID */
 	public static final int Table_ID = 116;
 
+	/** 項目 : 参照変数情報ID */
+	public static final String COLUMNNAME_FD_Reference_Param_ID = Table_Name + "_ID";
+	public static final String NAME_FD_Reference_Param_ID = Table_Disp_Name + "ID";
+	public static final String COMMENT_FD_Reference_Param_ID = "参照変数を識別しる為の情報ID";
+	/** 項目 : 参照変数名 */
+	public static final String COLUMNNAME_FD_Reference_Param_Name = Table_Name + "_Name";
+	public static final String NAME_FD_Reference_Param_Name = "参照変数名";
+	public static final String COMMENT_FD_Reference_Param_Name = "参照変数を識別しる為の名称";
+	/** 項目 : 参照情報ID */
+	public static final String COLUMNNAME_FD_Reference_ID = I_FD_Reference.COLUMNNAME_FD_Reference_ID;
+	public static final String NAME_FD_Reference_ID = I_FD_Reference.NAME_FD_Reference_ID;
+	/** 項目 : 属性項目情報ID */
+	public static final String COLUMNNAME_FD_TypeItem_ID = I_FD_TypeItem.COLUMNNAME_FD_TypeItem_ID;
+	public static final String NAME_FD_TypeItem_ID = I_FD_TypeItem.NAME_FD_TypeItem_ID;
+	/** 項目 : 数値情報 */
+	public static final String COLUMNNAME_FD_Reference_Param_Numeric = "FD_Reference_Param_Numeric";
+	public static final String NAME_FD_Reference_Param_Numeric = "数値情報";
+	public static final String COMMENT_FD_Reference_Param_Numeric = "数値変数として保管する為の項目";
 }
