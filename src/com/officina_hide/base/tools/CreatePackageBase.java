d280b75e6286db7d94829ada1153666b423d9ccf
DESKTOP-9U6NJ3E\hideo <hideo@uenotks.net>
2022-03-19 10:49:34 +0900

    プロジェクト基盤情報の生成用クラスの構築開始
    Start building class for generating project infrastructure information.

---
+++ src/com/officina_hide/base/tools/CreatePackageBase.java
@@ -0,0 +1,21 @@
+package com.officina_hide.base.tools;
+
+/**
+ * プロジェクトの基盤情報を構築する。<br>
+ * Build basic information for the project.<br>
+ * 引数 : 1-処理レベル<br>
+ * @author officina-hide.net
+ * @version 1.50 新規作成
+ * @since 2022/03/19 Ver. 1.50
+ */
+public class CreatePackageBase {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//処理レベルを引数から取得する。
+		System.out.println(args[0]);
+	}
+
+}
