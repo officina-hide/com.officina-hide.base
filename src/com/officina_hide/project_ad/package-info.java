366fa0ad29ee7d6e7ad268470694d5672eda4b2f
hideo ueno <hideo.ueno@kacoms.co.jp>
2022-03-30 17:58:58 +0900

    プロジェクトのセンタ工開発に際して、ブランチを作成する。
    Create a branch when developing the center of the project.

---
+++ src/com/officina_hide/project_ad/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * <p>プロジェクト管理を先行して開発する。<br>
+ * Baseの進捗に合わせてプロジェクトの構成を標準化する。<br></p>
+ * <p>Develop project management in advance.<br>
+ * Standardize the project structure as the Base progresses.<br></p>
+ * @author officina-hide.net
+ * @version 1.00
+ * @since 2022/03/30 Ver. 1.00
+ */
+package com.officina_hide.project_ad;
