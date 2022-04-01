90133bc6be99a25f574ada131b0d5fff19969425
hideo ueno <hideo.ueno@kacoms.co.jp>
2022-04-01 16:55:04 +0900

    Updated

---
+++ src/com/officina_hide/project_ad/tools/CreateProjectAD.java
@@ -0,0 +1,17 @@
+package com.officina_hide.project_ad.tools;
+
+import java.util.Date;
+
+/**
+ * プロジェクト管理構成クラス[Project management configuration class]<br>
+ * @author officina-hide.net
+ * @version 1.00 新規作成
+ * @since 2022/03/31 Ver. 1.00
+ */
+public class CreateProjectAD {
+
+	public static void main(String[] args) {
+		System.out.println("あProject management configuration start : "+new Date());
+	}
+
+}
---
+++ src/com/officina_hide/project_ad/tools/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * <p>プロジェクト管理を構成するための各種ツールクラスを管理する。<br>
+ * Manage various tool classes for configuring project management.<br></p>
+ * @author officina-hide.net
+ * @version 1.00 新規作成
+ * @since 2022/03/31 Ver. 1.00
+ */
+package com.officina_hide.project_ad.tools;
