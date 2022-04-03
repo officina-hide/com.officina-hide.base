a34f6812909cb7e5cd0dfe67f3004fbe195572df
DESKTOP-9U6NJ3E\hideo <hideo@uenotks.net>
2022-04-03 12:59:08 +0900

    module-info.java追加、JavaFx機能

--- src/module-info.java
+++ src/module-info.java
@@ -1,8 +1,6 @@
 module com.officina_hide {
-	requires javafx.controls;
-	requires java.sql;
 	requires javafx.graphics;
-	requires javafx.base;
-
+	requires java.sql;
+	
 	opens com.officina_hide.project_ad.fx;
 }
