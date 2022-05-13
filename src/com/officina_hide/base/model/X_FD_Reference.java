4f8d20a3cc00462311c9a415d88bf1784fdb3588
hideo ueno <hideo.ueno@kacoms.co.jp>
2022-05-13 17:33:03 +0900

    Updated

--- src/com/officina_hide/base/model/X_FD_Reference.java
+++ src/com/officina_hide/base/model/X_FD_Reference.java
@@ -11,11 +11,23 @@ import com.officina_hide.base.common.FD_EnvData;
 public class X_FD_Reference extends FD_DB implements I_FD_Reference {
 
 	/**
-	 * コンストラクター[
+	 * コンストラクター[Constructor]<br>
+	 * @author officina-hide.net
+	 * @since 2022/05/13 Ver. 1.50
 	 * @param env
 	 * @param entry
 	 */
 	public X_FD_Reference(FD_EnvData env, FD_Collections entry) {
+		createColumnList();
+	}
+
+	/**
+	 * 参照情報テーブル項目リスト生成[Reference information table Item list generation]<br>
+	 * @author officina-hide.net
+	 * @since 2022/05/13 Ver. 1.50
+	 */
+	private void createColumnList() {
+		columnCollection.clear();
 		
 	}
 
